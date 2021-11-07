ddd10237e2ca4c5af6f1b5443cf61a0851e9b416
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-11-07 13:51:06 -0300

    change do array reduce to find largest number in array

--- 01-filterArray.js
+++ 01-filterArray.js
@@ -1,18 +1,12 @@
-const arrayToFilter = ["a", 10, "b", "hola", 122, 15];
-
 const filterArray = (array) => {
   const letters = array
     .filter((item) => typeof item === "string")
     .map((item) => (item.length > 1 ? item.split("") : item))
     .flat(1);
-
   const uniqueLetters = letters.filter((item, i) => letters.indexOf(item) == i);
-  const numbers = array.filter((item) => typeof item === "number");
 
-  let largestNumber = numbers[0];
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > largestNumber) largestNumber = numbers[i];
-  }
+  const numbers = array.filter((item) => typeof item === "number");
+  const largestNumber = numbers.reduce((a, b) => Math.max(a, b));
 
   console.log(`Results:
   Letters: ${letters}
@@ -21,5 +15,7 @@ const filterArray = (array) => {
   Largest number: ${largestNumber}
   `);
 };
+
+const arrayToFilter = ["a", 10, "b", "hola", 122, 15];
 
 filterArray(arrayToFilter);
