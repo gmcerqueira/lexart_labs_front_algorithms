a7f2b2602e91ee56c9e9d3600dfc9dd221392c98
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-11-07 13:45:52 -0300

    show unique letters on filterArray func

--- 01-filterArray.js
+++ 01-filterArray.js
@@ -1,7 +1,12 @@
 const arrayToFilter = ["a", 10, "b", "hola", 122, 15];
 
 const filterArray = (array) => {
-  const letters = array.filter((item) => typeof item === "string");
+  const letters = array
+    .filter((item) => typeof item === "string")
+    .map((item) => (item.length > 1 ? item.split("") : item))
+    .flat(1);
+
+  const uniqueLetters = letters.filter((item, i) => letters.indexOf(item) == i);
   const numbers = array.filter((item) => typeof item === "number");
 
   let largestNumber = numbers[0];
@@ -11,6 +16,7 @@ const filterArray = (array) => {
 
   console.log(`Results:
   Letters: ${letters}
+  Unique letters: ${uniqueLetters}
   Numbers: ${numbers}
   Largest number: ${largestNumber}
   `);
